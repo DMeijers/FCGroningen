4c123440e632e1949c9793a596797dd59a06b32c
Dieko Meijers <admeijers@hotmail.nl>
2022-12-02 23:09:48 +0100

    Added hands, movement and teleportation

---
+++ Assets/Scripts/ActivateRay.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+public class ActivateRay : MonoBehaviour
+{
+    // The right hand teleportation gameobject
+    public GameObject rightTeleportation;
+
+    // The input of hand teleportation 
+    public InputActionProperty rightActive;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Checks if the trigger value of the teleportation is above 0.1f
+        rightTeleportation.SetActive(rightActive.action.ReadValue<float>() > 0.1f);
+    }
+}
