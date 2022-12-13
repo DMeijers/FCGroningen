62dbb22b8cb22a564e0d88f55fc81c31f28676e6
Dieko Meijers <admeijers@hotmail.nl>
2022-12-13 15:21:34 +0100

    Game Menu and grab ray interaction added
    Game menu working on the menu button and a grab ray to get an object from a distance

--- Assets/Scripts/ActivateRay.cs
+++ Assets/Scripts/ActivateRay.cs
@@ -9,18 +9,22 @@ public class ActivateRay : MonoBehaviour
     // The right hand teleportation gameobject
     public GameObject rightTeleportation;
 
-    // The input of hand teleportation 
+    // The input of hand teleportation to activate 
     public InputActionProperty rightActive;
 
-    // The input of hand teleportation
+    // The input of hand teleportation to cancel
     public InputActionProperty rightCancel;
+
+    // The input of hand ray
+    public XRRayInteractor rightRay;
 
 
 
     // Update is called once per frame
     void Update()
     {
+        bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 rightNormal, out int rightNumber, out bool rightValid); 
         // Checks if the trigger value of the teleportation is above 0.1f
-        rightTeleportation.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightActive.action.ReadValue<float>() > 0.1f);
+        rightTeleportation.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightActive.action.ReadValue<float>() > 0.1f);
     }
 }
