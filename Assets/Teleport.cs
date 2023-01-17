d4d785ab5bef59d501147bd4036d92297a9518aa
Dieko Meijers <admeijers@hotmail.nl>
2023-01-17 21:45:54 +0100

    Teleportation fix

--- Assets/Teleport.cs
+++ Assets/Teleport.cs
@@ -7,19 +7,12 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Teleport : MonoBehaviour
 {
     
-    XRGrabInteractable m_InteractableBase;
     public int sceneIndex = 1;
     
-    // Start is called before the first frame update
-    protected void Start()
-    {
-        m_InteractableBase = GetComponent<XRGrabInteractable>();
-        m_InteractableBase.selectEntered.AddListener(PickedUp);
-    }
-    
     // Update is called once per frame
-    void PickedUp(SelectEnterEventArgs args)
+    public void PickedUp()
     {
+        Debug.Log("stuff works");
         SceneManager.LoadScene(sceneIndex);
     }
 }
